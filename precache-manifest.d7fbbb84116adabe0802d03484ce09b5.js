2a1a05c0acf72c6d182a58e0cb8317c2353a158c
Postimal <michal.postek1@wp.pl>
2020-01-08 12:43:04 +0100

    Updates

---
+++ precache-manifest.d7fbbb84116adabe0802d03484ce09b5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75094748c7896b16be37a96fe05dad44",
+    "url": "/react-moviedb/index.html"
+  },
+  {
+    "revision": "53d4158097bb4981159b",
+    "url": "/react-moviedb/static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "9cdf61ab770f6bcf5a7e",
+    "url": "/react-moviedb/static/css/main.f044d1b2.chunk.css"
+  },
+  {
+    "revision": "53d4158097bb4981159b",
+    "url": "/react-moviedb/static/js/2.ae6bca98.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-moviedb/static/js/2.ae6bca98.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9cdf61ab770f6bcf5a7e",
+    "url": "/react-moviedb/static/js/main.96647e41.chunk.js"
+  },
+  {
+    "revision": "15dfc855f631de7a7542",
+    "url": "/react-moviedb/static/js/runtime-main.b2bef974.js"
+  },
+  {
+    "revision": "784c6ad2f5f58f2ffb439a4710683b28",
+    "url": "/react-moviedb/static/media/cinema-clapboard.784c6ad2.png"
+  },
+  {
+    "revision": "15bc6a4f9365525406b1d27d69f3525e",
+    "url": "/react-moviedb/static/media/error404.15bc6a4f.jpg"
+  },
+  {
+    "revision": "15d0b9d36ea097059f74c9d24c4f5f50",
+    "url": "/react-moviedb/static/media/hacker-avatar.15d0b9d3.png"
+  },
+  {
+    "revision": "e118fa6dcfcb75c5bc7548aeaa5c577e",
+    "url": "/react-moviedb/static/media/spinner_b.e118fa6d.webp"
+  },
+  {
+    "revision": "dbe89c91cfecaa3f5838432d2d54859b",
+    "url": "/react-moviedb/static/media/star.dbe89c91.png"
+  }
+]);
